e4f5b2a2592491f6d6314c88d4140f22f9e97369
Mohamed Sami <melsatar@gmail.com>
2013-01-12 01:20:24 +0200

    adding item amount value at scanning
    checking for value campared with available at stock

--- src/com/virtual/market/Item_Scanning.java
+++ src/com/virtual/market/Item_Scanning.java
@@ -8,7 +8,10 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import android.R.integer;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -16,6 +19,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,6 +27,7 @@ import android.widget.Toast;
 public class Item_Scanning extends Activity {
 	String Product_ID;
 	String Product_price;
+	int Product_amount;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,7 @@ public class Item_Scanning extends Activity {
 		final ImageView Item_Image = (ImageView) findViewById(R.id.item_image);
 		final TextView Item_Cat = (TextView) findViewById(R.id.item_cat);
 		final TextView Item_Price = (TextView) findViewById(R.id.item_price);
+		final TextView item_amount = (TextView) findViewById(R.id.item_amount);
 		final Button btn_AddtoCart = (Button) findViewById(R.id.addtoCart);
 		final Button btn_AddanotherPro = (Button) findViewById(R.id.addanthr);
 		final ParseUser currentuser = ParseUser.getCurrentUser();
@@ -58,13 +64,16 @@ public class Item_Scanning extends Activity {
 						if (e == null) {
 
 							Product_price = object.getString("item_price");
+							Product_amount = Integer.valueOf(object.getNumber(
+									"item_amount").toString());
 							Item_ID.setText(" Product ID : " + Product_ID);
 							Item_title.setText(" Product : "
 									+ object.getString("item_name"));
 							Item_Cat.setText(" Category : "
 									+ object.getString("item_desc"));
 							Item_Price.setText(" Price : " + Product_price);
-
+							item_amount.setText("Available amount= "
+									+ Product_amount);
 							ParseFile image = (ParseFile) object
 									.get("item_image");
 							image.getDataInBackground(new GetDataCallback() {
@@ -107,21 +116,30 @@ public class Item_Scanning extends Activity {
 
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				EditText txt_amount = (EditText) findViewById(R.id.txt_amount);
+				
+				String amount = txt_amount.getText().toString();
+				if (!amount.isEmpty()) {
+					if (Integer.valueOf(amount) <= Product_amount) {
+						ParseObject Cart = new ParseObject("Cart");
+						Cart.put("item_amount", Integer.valueOf(amount));
+						Cart.put("item_id", Product_ID);
+						Cart.put("user_id", currentuser.getObjectId());
+						Cart.saveInBackground();
+						ShowDialogBox("Item has been added to your cart");
+					} else {
+						ShowDialogBox("the required amount is not available on stock");
+					}
 
-				ParseObject Cart = new ParseObject("Cart");
-				Cart.put("item_amount", Integer.parseInt(Product_price));
-				Cart.put("item_id", Product_ID);
-				Cart.put("user_id", currentuser.getObjectId());
-				Cart.saveInBackground();
-
-				Toast.makeText(getBaseContext(), "Item has been added",
-						Toast.LENGTH_SHORT).show();
+				} else {
+					ShowDialogBox("Please add item quantity");
+				}
 
 			}
 		};
 
 		btn_AddtoCart.setOnClickListener(lsn_add);
-		
+
 		OnClickListener lsn_cart = new OnClickListener() {
 
 			public void onClick(View v) {
@@ -129,7 +147,6 @@ public class Item_Scanning extends Activity {
 				Intent cart_intent = new Intent();
 				cart_intent.setClass(Item_Scanning.this, Historical_Data.class);
 				startActivity(cart_intent);
-			
 
 			}
 		};
@@ -140,6 +157,26 @@ public class Item_Scanning extends Activity {
 
 	void setProduct_Price(String PP) {
 		this.Product_price = PP;
+	}
+
+	private void ShowDialogBox(String message) {
+
+		AlertDialog myQuittingDialogBox =
+
+		new AlertDialog.Builder(this)
+				.setTitle("Notification")
+				.setMessage(message)
+
+				.setPositiveButton("Okay",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+							}
+						})
+
+				.create();
+
+		myQuittingDialogBox.show();
 	}
 
 }
